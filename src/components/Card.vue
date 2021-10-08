359b420df5acb149520b989b40fb40ca6ba3a9ef
Angelo7x <abennici96@gmail.com>
2021-10-08 17:20:15 +0200

    Inserito hover con info, sistemare layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="card">
-      <h2>{{attr.title ? attr.title : attr.name}}</h2>
-      <h2>{{attr.original_title ? attr.original_title : attr.original_name}}</h2>
-      <h2>{{attr.original_language}}</h2>
-      <h2>{{attr.vote_average}}</h2>
-      <img :src="`https://image.tmdb.org/t/p/w342/${attr.poster_path}`" alt="copertina">
+  <div class="card-container">
+      <img class="card-poster" :src="`https://image.tmdb.org/t/p/w342/${attr.poster_path}`" alt="poster">
+    <div class="card-info">
+      <h2>Titolo : {{attr.title ? attr.title : attr.name}}</h2>
+      <h2 class="opacity">Titolo originale : {{attr.original_title ? attr.original_title : attr.original_name}}</h2>
+      <h2>Lingua originale : {{attr.original_language}}</h2>
+      <h2>Voto : {{attr.vote_average}}</h2>
+      <h2 class="overview opacity">Overview : {{attr.overview}}</h2>
+    </div>
   </div>
 </template>
 
@@ -17,11 +20,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card {
-  display: flex;
-  h2 {
-    padding: 10px;
+.card-container {
+  position: relative;
+  color: white;
+
+  &:hover .card-info {
+    opacity: 1;
   }
 
+  .card-info {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    opacity: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 30%;
+    height: 100%;
+    overflow-y: scroll;
+    h2 {
+      padding: 20px;
+    }
+
+    .opacity {
+      opacity: 0.6;
+    }
+
+  }
 }
 </style>
